238e8ab264cbdb894bfe77790288a5be1ba0cb7d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-18 19:13:31 +0530

    Revamp test suite with in-memory database (h2)

--- back-end/src/test/java/lk/ijse/dep13/backend/data/CustomerDataAccessTest.java
+++ back-end/src/test/java/lk/ijse/dep13/backend/data/CustomerDataAccessTest.java
@@ -4,23 +4,53 @@ import lk.ijse.dep13.backend.to.Customer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 // Test Suite
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class CustomerDataAccessTest {
 
     Connection connection;
 
     @BeforeEach
-    void setUp() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dep13_customers_db",
-                "postgres", "psql");
+    void setUp() throws SQLException, ClassNotFoundException {
+        Class.forName("org.h2.Driver");
+        connection = DriverManager.getConnection(
+                "jdbc:h2:mem:test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DEFAULT_NULL_ORDERING=HIGH",
+                "", "");
+        executeScripts();
+    }
+
+    private void executeScripts(){
+        try(BufferedReader schemeBr = new BufferedReader(new InputStreamReader
+                (Objects.requireNonNull(getClass().getResourceAsStream("/schema.sql"))))){
+            StringBuilder schemaScript = new StringBuilder();
+            schemeBr.lines().forEach(schemaScript::append);
+            try(var stm = connection.createStatement()){
+                stm.execute(schemaScript.toString());
+            }
+
+            Path path = Path.of(Objects.requireNonNull(getClass().getResource("/data.sql")).toURI());
+            String dataScript = Files.readString(path);
+            try(var stm = connection.createStatement()){
+                stm.execute(dataScript);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @AfterEach
