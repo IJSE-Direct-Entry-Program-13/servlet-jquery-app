dd60ccc956d0de2a13fb5a6e8d76012b3e23c124
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-19 21:01:26 +0530

    Implement delete

--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -17,14 +17,14 @@ function loadAllCustomers() {
                     $("#tbl-customers tfoot").addClass('d-none');
                     customerList.forEach(c =>{
                         $('#tbl-customers tbody').append(`
-                    <tr>
+                    <tr class="animate__animated animate__fadeIn">
                         <td>
                             <div class="profile-picture rounded border"
                                  style="background-image: url('${c.profilePicture}')"></div>
                         </td>
                         <td>
                             <div>
-                                <b>ID:</b> ${c.id}
+                                <b>ID:</b> <span class="c-id">${c.id}</span>
                             </div>
                             <div>
                                 <b>Name:</b> ${c.name}
@@ -45,7 +45,7 @@ function loadAllCustomers() {
             } else {
                 alert('Something went wrong, try again');
             }
-        }, 1000);
+        }, 500);
     });
 
     // 3.
@@ -57,3 +57,16 @@ function loadAllCustomers() {
     xhr.send();
 
 }
+
+$('#tbl-customers tbody').on('click', ".bi.bi-trash", (e)=>{
+    const id = $(e.target).parents("tr").find('span.c-id').text();
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('loadend', ()=>{
+
+    });
+    xhr.open('DELETE',
+        `http://localhost:8080/app/v1/customers/${id}`,
+        true);
+    xhr.send();
+});
